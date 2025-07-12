d7e1ace1c04d6fd7df515f59f31e9247900a74d0
yourgrok <sharmaudhalak@gmail.com>
2025-07-12 22:27:13 +0530

    Set favicon and tab title

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'Renn India | Video Editor',
+        },
+      },
+    }),
+  ],
   css: {
     postcss: './postcss.config.js',
   },
